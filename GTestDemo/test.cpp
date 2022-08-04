423fb2af7ab085d9857d2a4a0ee90ed956deab4e
Nidhal <nidhalmesselmani@yahoo.fr>
2022-08-04 17:10:17 +0100

    first look into google test: arrange,act, assert and test fixtures

--- GTestDemo/test.cpp
+++ GTestDemo/test.cpp
@@ -1,6 +1,47 @@
 #include "pch.h"
+#include<iostream>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class Check 
+{
+	int val;
+public:
+	Check() : val(0) 
+	{
+
+	}
+
+	void setValue(int newVal) 
+	{
+		this->val = newVal;
+	}
+
+	int getValue() {
+		return this->val;
+	}
+};
+// Test Fixture
+struct TF : testing::Test 
+{
+	Check* c1;
+	void SetUp() { cout << "Setup" << endl;  c1 = new Check(); };
+	void TearDown() { cout << "Teardown" << endl; delete c1; };
+};
+
+TEST_F( TF, SubTestName ) {
+	
+	// Arrange
+	// Act
+	c1->setValue(100);
+	// Assert
+	ASSERT_EQ(c1->getValue(), 100);
+}
+
+TEST_F( TF, SubTestName_1) {
+
+	// Arrange
+	// Act
+	c1->setValue(500);
+	// Assert
+	ASSERT_EQ(c1->getValue(), 500);
 }
